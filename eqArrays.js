539ce719e9fd1ef197dae14a58faa347d918a4f7
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-06 11:00:28 -0500

    fix

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,9 @@
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) { // compare length of arr1 and arr2
+  if (arr1.length !== arr2.length) {
     console.log(false);
   } else {
-    for (let i = 0; i < arr1.length; i++) { // loop over both arr1 and arr2
-      if (arr1[i] !== arr2[i]) { // if two values don't match break loop and return false
+    for (let i = 0; i < arr1.length; i++) { 
+      if (arr1[i] !== arr2[i]) { 
         console.log(false)
         return
       }
