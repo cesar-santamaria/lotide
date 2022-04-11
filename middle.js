0e0c02c953d73e00b46f9b7bc71a110f8151a6e5
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-11 16:56:50 -0500

    refactor code for middle.js and create modules for middle.js and middleTest.js

--- middle.js
+++ middle.js
@@ -1,10 +1,5 @@
-/* 
-The middle function should return an array with only the middle element(s) of the provided array. This means that the length of the returned elements could vary.
+const assertArraysEqual = require('./assertArraysEqual')
 
-For arrays with one or two elements, there is no middle. Return an empty array.
-*/
-
-//create function
 const middle = (arr) =>{
   //create a variable to store middle values
   let middleValues = []
@@ -14,15 +9,14 @@ const middle = (arr) =>{
   }
   //create variable to store the index of the middle variable of array input.
   const middleIndex = Math.floor(arr.length / 2);
-  console.log(middleIndex)
   //conditional to check the length of array and push multiple middle index values if true. 
   if (arr.length % 2 === 0){
     middleValues.push(arr[middleIndex - 1])
-    console.log(middleValues)
   }
   middleValues.push(arr[middleIndex])
-  return middleValues
-
+  return parseInt(middleValues)
 }
 
-console.log(middle([1,2,3,4,5]))
+middle([1,2,3,4,5,])
+module.exports = middle;
+
---
+++ test/middleTest.js
@@ -0,0 +1,5 @@
+const assertEqualArrays = require('../assertArraysEqual');
+const middle = require('../middle')
+
+//TEST CODE
+assertEqualArrays(middle([1,2,3,4,5]),3)
