7c2378fa277fa9e51e2b9c24479b449e35d9fe4d
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-11 15:45:03 -0500

    refactored code for head.js and created a module for both head.js and assertEqual.js

--- head.js
+++ head.js
@@ -1,15 +1,9 @@
 // ASSERTION TEST
-const assertEqual = function(actual, expected) {
-  if (actual === expected) return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  if (actual !== expected) return console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 // Create a function that returns the first value in an array
 const head = (arr) => {
   return arr[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-
+module.exports = head;
